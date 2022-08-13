410d7b79785aba2234f1e0a8858ba98ad40841dd
MINEDUCYT <MINEDUCYT@DESKTOP-O47S48O>
2022-08-12 20:52:35 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nota promedio");
+            double nota = double.Parse(Console.ReadLine());
+
+            if (nota >= 0 && nota <= 10)
+            {
+                if (nota >= 6)
+                {
+                    Console.WriteLine("Aprobado");
+                }
+                else if (nota <= 5)
+                {
+                    Console.WriteLine("Reposicion");
+                }
+                else
+                {
+                    Console.WriteLine("Reprobado");
+                }
+            }
+        }
+    }
+}
---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+            string linea;
+            do
+            {
+                Console.WriteLine("Ingrese un valor entre 0 y 999 (0 finaliza):");
+                linea = Console.ReadLine();
+                valor = int.Parse(linea);
+                if (valor >= 100)
+                {
+                    Console.WriteLine("Tiene 3 digitos.");
+                }
+                else if (valor >= 10)
+                {
+                    Console.WriteLine("Tiene 2 digitos.");
+                }
+                else
+                {
+                    Console.WriteLine("Tiene un digito.");
+                }
+
+            } while (valor != 0);
+        }
+    }
+}
